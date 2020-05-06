d9659eab177596e343d2250362ab11c2874ae6e5
soypat <graded.sp@gmail.com>
2020-05-06 20:22:29 -0300

    add quick6S setup

--- public/python/spacemeters.py
+++ public/python/spacemeters.py
@@ -52,6 +52,18 @@ def init6SLinux():
     sh('./build/6SV1.1/sixsV1.1 < ./build/Examples/Example_In_1.txt') # Test binary
     sh('ln ./build/6SV1.1/sixsV1.1 /usr/local/bin/sixs') # Add 6S to $PATH environment variable
 
+"""
+    Crea el objeto SixS con opciones/parametros genericos para ahorrar espacio de codigo
+"""
+def quickSixS(dir="./build/6SV1.1/sixsV1.1"):
+    s = SixS(path=dir)
+    s.aero_profile = AeroProfile.PredefinedType(AeroProfile.Urban)
+    s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.Tropical)
+    s.geometry.from_time_and_location(-25.31, 33.23, "11:41", 14.9, 171.3)
+    s.altitudes.set_sensor_satellite_level()
+    s.ground_reflectance = GroundReflectance.HomogeneousLambertian(GroundReflectance.GreenVegetation)
+    return s
+
 # Regular functions
 
 def irradianceToPower(IR, altitudeSatellite , areaObsTierra , areaLente ): #iradiance from earth to power upstream the satellite lens
